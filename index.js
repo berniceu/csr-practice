6aed178db03f30d84e92db464b2ff383a246cad8
bernice uwituze <b.uwituze@alustudent.com>
2024-03-26 19:19:37 +0200

    Create index file and style using scss
    -Create index file for luxury(a furniture company) and link with stylesheet.
    -Import all images from figma design
    -Create header, section for why-us, contact, hero and trending

---
+++ index.js
@@ -0,0 +1,12 @@
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 0){
+        header.classList.add('nav-active');
+    } else{
+        header.classList.remove('nav-active');
+    }
+
+    
+
+})
